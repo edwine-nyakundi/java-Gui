4a26a3b0a604291d359741a816ccfc9ffc37d873
edwine-nyakundi <145298218+edwine-nyakundi@users.noreply.github.com>
2024-01-01 14:46:50 +0300

    button GUI

---
+++ ButtonDemo3.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+public class ButtonDemo3 {
+    public static void main(String[] args) {
+        Frame f2=new Frame("MARKTDAY.COM");
+        Button b2=new Button("REGISTER");
+        b2.setBounds(10, 50, 60, 70);
+        f2.add(b2);
+        f2.setSize(800, 800);
+        f2.setLayout(null);
+        f2.setVisible(true);
+    }
+}
